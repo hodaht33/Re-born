660ec2f2149cee5d626305f2605f1602d7698968
seo-hyeon <nabibaby@naver.com>
2020-08-03 08:47:07 +0900

    move2
    점프, 타일 간 이동 구현

---
+++ RebornGameProject/Assets/Scripts/MoveTrainCar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrainCar : MonoBehaviour
+{
+    public float move = 15f;
+    //GameObject parent = gameObject.transform.parent;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.Translate(new Vector3(move,0,0));
+            //parent.setColor();
+        }
+    }
+}
