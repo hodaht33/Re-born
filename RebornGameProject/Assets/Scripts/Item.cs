1de19d936f27b2948cf6df7d8e4593365f985718
seo-hyeon <nabibaby@naver.com>
2020-08-23 23:54:53 +0900

    inventory_1

---
+++ RebornGameProject/Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Item
+{
+    public string itemName;
+    public int itemID;
+    public string itemDes;
+    public Texture2D itemIcon;
+
+    public Item()
+    {
+
+    }
+
+    public Item(string itemName, int itemID, string itemDes)//, string img)
+    {
+        this.itemName = itemName;
+        this.itemID = itemID;
+        this.itemDes = itemDes;
+        //this.itemIcon = Resources.Load<Texture2D>("ItemIcons/34x34icons180709_" + img);
+    }
+}
---
+++ RebornGameProject/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        items.Add(new Item("A", 1, "aaa"));
+        items.Add(new Item("B", 2, "bbb"));
+        items.Add(new Item("C", 3, "ccc"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
