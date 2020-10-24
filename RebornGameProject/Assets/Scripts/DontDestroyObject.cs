147662fb6025c9174c69a3ec9cf908cfc6e8a102
hodaht33 <hodaht@naver.com>
2020-10-24 15:55:16 +0900

    201024

---
+++ RebornGameProject/Assets/Scripts/DontDestroyObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 작성자 : 이성호
+/// 기능 : 씬 변경 후에도 오브젝트 유지
+/// </summary>
+public class DontDestroyObject : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
