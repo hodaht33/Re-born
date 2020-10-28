85a0885768320ea7ad5db133f9da7d01510f7fa0
hodaht33 <hodaht@naver.com>
2020-10-28 17:07:20 +0900

    Revert "201028"
    This reverts commit 00ea73f7755f929bc6cf67db86a00e99d3945564.

--- RebornGameProject/Assets/Scripts/Managers/UIManager.cs
+++ RebornGameProject/Assets/Scripts/Managers/UIManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// 작성자 : 이성호
@@ -19,13 +21,6 @@ public class UIManager : SingletonBase<UIManager>
     //public event ClickExitButton OnClickExitButton;
     public event ClickExitButton OnClickDropdownExitButton;
 
-    private bool isActivateSettings = false;
-    public bool IsActivateSettings
-    {
-        get { return isActivateSettings; }
-        private set { }
-    }
-
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -42,13 +37,11 @@ public class UIManager : SingletonBase<UIManager>
     {
         if (Input.GetKeyDown(KeyCode.Escape) && currentEnableCanvas == null)
         {
-            isActivateSettings = true;
             OpenSettings();
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && currentEnableCanvas != null)
         {
             ExitMenu();
-            isActivateSettings = false;
         }
     }
 
@@ -66,11 +59,17 @@ public class UIManager : SingletonBase<UIManager>
     // ExitButton 이벤트에 적용
     public void ExitMenu()
     {
+        //UnityEngine.EventSystems.EventSystem.
+        //    current.currentSelectedGameObject.transform.
+        //    parent.gameObject.SetActive(false);
+
         if (currentEnableCanvas.Equals(settingsCanvas))
         {
             OnClickDropdownExitButton.Invoke();
         }
-        
+
+        //OnClickExitButton.Invoke();
+
         currentEnableCanvas.enabled = false;
         currentEnableCanvas = null;
         menuCanvas.enabled = true;
