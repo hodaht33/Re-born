daa564518b80ec89272a447d334ab496c77f7a4e
hodaht33 <hodaht@naver.com>
2020-11-14 21:25:21 +0900

    rev?

--- RebornGameProject/Assets/Scripts/DontDestroyObjects/StartChat.cs
+++ RebornGameProject/Assets/Scripts/DontDestroyObjects/StartChat.cs
@@ -19,9 +19,34 @@ public class StartChat : MonoBehaviour
     // EventTrigger사용할 경우 사용
     public void Click()
     {
+        if (UIManager.Instance.IsActivateSettings == false & state != 2)
+        {
+            GameObject large = GameObject.Find("Canvas").transform.Find("LargeImg").gameObject;
+
+            if (large.activeSelf == false)
+            {
+                if (currentClickCountImg < LargeImgs.Length)
+                {
+                    if (LargeImgs[currentClickCountImg] != null)
+                    {
+                        large.SetActive(true);
+                        large.transform.GetComponent<Image>().sprite = LargeImgs[currentClickCountImg];
+                        GameObject.Find("Canvas").transform.Find("back_gray").gameObject.SetActive(true);
+                    }
+                    ++currentClickCountImg;
+                }
+                else
+                {
+                    large.SetActive(true);
+                    large.transform.GetComponent<Image>().sprite = LargeImgs[currentClickCountImg - 1];
+                    GameObject.Find("Canvas").transform.Find("back_gray").gameObject.SetActive(true);
+                }
+            }
+        }
 
         if (Chat.Instance.IsActivateChat == false & state != 1)
         {
+<<<<<<< HEAD
             if (currentClickCount >= texts.Length)
                 currentClickCount = texts.Length - 1;
             if (currentClickCountImg >= LargeImgs.Length)
@@ -34,14 +59,53 @@ public class StartChat : MonoBehaviour
 
             ++currentClickCount;
             ++currentClickCountImg;
+=======
+            if (currentClickCount < texts.Length)
+            {
+                Chat.Instance.ActivateChat(texts[currentClickCount]);
+                ++currentClickCount;
+            }
+            else
+            {
+                Chat.Instance.ActivateChat(texts[currentClickCount - 1]);
+            }
+>>>>>>> parent of 1ef217c... chat
         }
     }
 
     // Collider를 넣어 사용 할 수 있는 3D오브젝트가 사용
     public void OnMouseDown()
     {
+
+        if (UIManager.Instance.IsActivateSettings == false & state != 2)
+        {
+            GameObject large = GameObject.Find("Canvas").transform.Find("LargeImg").gameObject;
+
+            if (large.activeSelf == false)
+            {
+                if (currentClickCountImg < LargeImgs.Length)
+                {
+                    if (LargeImgs[currentClickCountImg] != null)
+                    {
+                        large.SetActive(true);
+                        large.transform.GetComponent<Image>().sprite = LargeImgs[currentClickCountImg];
+                        GameObject.Find("Canvas").transform.Find("back_gray").gameObject.SetActive(true);
+                    }
+                    ++currentClickCountImg;
+                }
+                else
+                {
+                    large.SetActive(true);
+                    large.transform.GetComponent<Image>().sprite = LargeImgs[currentClickCountImg - 1];
+                    GameObject.Find("Canvas").transform.Find("back_gray").gameObject.SetActive(true);
+                }
+            }
+        }
+
+
         if (Chat.Instance.IsActivateChat == false & state != 1)
         {
+<<<<<<< HEAD
             if (currentClickCount >= texts.Length)
                 currentClickCount = texts.Length - 1;
             if (currentClickCountImg >= LargeImgs.Length)
@@ -54,6 +118,17 @@ public class StartChat : MonoBehaviour
 
             ++currentClickCount;
             ++currentClickCountImg;
+=======
+            if (currentClickCount < texts.Length)
+            {
+                Chat.Instance.ActivateChat(texts[currentClickCount]);
+                ++currentClickCount;
+            }
+            else
+            {
+                Chat.Instance.ActivateChat(texts[currentClickCount - 1]);
+            }
+>>>>>>> parent of 1ef217c... chat
         }
     }
 }
