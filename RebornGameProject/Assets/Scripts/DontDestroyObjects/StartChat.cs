bd4e85daadf6eadd9ed207c590524d918ac76c55
hodaht33 <hodaht@naver.com>
2020-11-14 22:20:23 +0900

    Fix

--- RebornGameProject/Assets/Scripts/DontDestroyObjects/StartChat.cs
+++ RebornGameProject/Assets/Scripts/DontDestroyObjects/StartChat.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 작성자 : 이성호
@@ -11,38 +12,52 @@ public class StartChat : MonoBehaviour
     [SerializeField]
     private string[] texts;
     private int currentClickCount = 0;
+    private int currentClickCountImg = 0;
+    public Sprite[] LargeImgs;
+    public int state = 0; // 0: chat + image, 1: image, 2: chat
 
     // EventTrigger사용할 경우 사용
     public void Click()
     {
-        if (Chat.Instance.IsActivateChat == false)
+
+        if (Chat.Instance.IsActivateChat == false & state != 1)
         {
-            if (currentClickCount < texts.Length)
-            {
+            if (currentClickCount >= texts.Length)
+                currentClickCount = texts.Length - 1;
+            if (currentClickCountImg >= LargeImgs.Length)
+                currentClickCountImg = LargeImgs.Length - 1;
+
+            if (texts[currentClickCount] != null & LargeImgs[currentClickCount] != null)
+                Chat.Instance.ActivateChat(texts[currentClickCount], LargeImgs[currentClickCount]);
+            else if (texts[currentClickCount] != null)
                 Chat.Instance.ActivateChat(texts[currentClickCount]);
-                ++currentClickCount;
-            }
-            else
-            {
-                Chat.Instance.ActivateChat(texts[currentClickCount - 1]);
-            }
+            else if (LargeImgs[currentClickCount] != null)
+                Chat.Instance.ActivateChat(LargeImgs[currentClickCount]);
+
+            ++currentClickCount;
+            ++currentClickCountImg;
         }
     }
 
     // Collider를 넣어 사용 할 수 있는 3D오브젝트가 사용
     public void OnMouseDown()
     {
-        if (Chat.Instance.IsActivateChat == false)
+        if (Chat.Instance.IsActivateChat == false & state != 1)
         {
-            if (currentClickCount < texts.Length)
-            {
+            if (currentClickCount >= texts.Length)
+                currentClickCount = texts.Length - 1;
+            if (currentClickCountImg >= LargeImgs.Length)
+                currentClickCountImg = LargeImgs.Length - 1;
+
+            if (texts[currentClickCount] != "" & LargeImgs[currentClickCount] != null)
+                Chat.Instance.ActivateChat(texts[currentClickCount], LargeImgs[currentClickCount]);
+            else if (texts[currentClickCount] != "")
                 Chat.Instance.ActivateChat(texts[currentClickCount]);
-                ++currentClickCount;
-            }
-            else
-            {
-                Chat.Instance.ActivateChat(texts[currentClickCount - 1]);
-            }
+            else if (LargeImgs[currentClickCount] != null)
+                Chat.Instance.ActivateChat(LargeImgs[currentClickCount]);
+
+            ++currentClickCount;
+            ++currentClickCountImg;
         }
     }
 }
