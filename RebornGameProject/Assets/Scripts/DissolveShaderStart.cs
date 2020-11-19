224418f99a6572b2076911eff133c6ecd9737f81
seo-hyeon <nabibaby@naver.com>
2020-11-20 02:58:10 +0900

    MainMenu 수정

---
+++ RebornGameProject/Assets/Scripts/DissolveShaderStart.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DissolveShaderStart : MonoBehaviour
+{
+    private Material mat;
+
+    [SerializeField]
+    private float speed = 0.5f;
+    
+    private void Awake()
+    {
+        mat = GetComponent<Text>().material;
+    }
+
+    /*
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+                mat[currentEffectNum].SetFloat("_Edges", 0.0f);
+                mat[currentEffectNum].SetFloat("_Level", 0.0f);
+            }
+
+            imageObject[currentEffectNum].SetActive(false);
+
+            if (currentEffectNum != 0)
+            {
+                --currentEffectNum;
+            }
+
+            text.text = (currentEffectNum + 1).ToString();
+            imageObject[currentEffectNum].SetActive(true);
+
+            mat[currentEffectNum].SetFloat("_Edges", 0.0f);
+            mat[currentEffectNum].SetFloat("_Level", 0.0f);
+
+            currentCoroutine = StartCoroutine(ChangeShaderValue());
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+                mat[currentEffectNum].SetFloat("_Edges", 0.0f);
+                mat[currentEffectNum].SetFloat("_Level", 0.0f);
+            }
+
+            imageObject[currentEffectNum].SetActive(false);
+
+            if (currentEffectNum != mat.Length - 1)
+            {
+                ++currentEffectNum;
+            }
+
+            text.text = (currentEffectNum + 1).ToString();
+            imageObject[currentEffectNum].SetActive(true);
+
+            mat[currentEffectNum].SetFloat("_Edges", 0.0f);
+            mat[currentEffectNum].SetFloat("_Level", 0.0f);
+
+            currentCoroutine = StartCoroutine(ChangeShaderValue());
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+            }
+
+            mat[currentEffectNum].SetFloat("_Edges", 0.0f);
+            mat[currentEffectNum].SetFloat("_Level", 0.0f);
+
+            currentCoroutine = StartCoroutine(ChangeShaderValue());
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            bool activate = false;
+
+            if (textsObject[0].activeInHierarchy == false)
+            {
+                activate = true;
+            }
+
+            for (int i = 0; i < textsObject.Length; ++i)
+            {
+                textsObject[i].SetActive(activate);
+            }
+        }
+    }*/
+
+    public IEnumerator ChangeShaderValue()
+    {
+        float deltaVal = 1.0f;
+
+        yield return new WaitForSeconds(1.0f);
+
+        while (deltaVal > 0.01f)
+        {
+            deltaVal -= Time.deltaTime * speed;
+            mat.SetFloat("_Level", deltaVal);
+
+            yield return null;
+        }
+
+        //deltaVal = 0.0f;
+
+        //while (deltaVal <= 1.0f)
+        //{
+        //    deltaVal += Time.deltaTime * speed;
+        //    mat[currentEffectNum].SetFloat("_Level", deltaVal);
+
+        //    yield return null;
+        //}
+    }
+
+    private void OnDestroy()
+    {
+        mat.SetFloat("_Edges", 1.0f);
+        mat.SetFloat("_Level", 1.0f);
+    }
+}
--- RebornGameProject/Assets/Scripts/Start/StartClassroom.cs
+++ RebornGameProject/Assets/Scripts/Start/StartClassroom.cs
@@ -5,8 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class StartClassroom : MonoBehaviour
 {
+    private Coroutine tickCoroutine = null;
+    [SerializeField]
+    private float activateTime = 10.0f;
+    [SerializeField]
+    private Canvas texts;
+
+    /*
     public void onClick()
     {
         SceneManager.LoadScene("Chapter1");
+    }*/
+
+    private void Start()
+    {
+        tickCoroutine = StartCoroutine(TickActivateTime());
+        texts.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Chapter1");
+        }
+    }
+
+    private IEnumerator TickActivateTime()
+    {
+        float tickTime = 0.0f;
+
+        while (tickTime <= activateTime)
+        {
+            tickTime = tickTime + Time.deltaTime;
+
+            yield return null;
+        }
+
+        ShowText();
+    }
+
+    private void ShowText()
+    {
+        texts.enabled = true;
+        StartCoroutine(texts.transform.Find("Title").gameObject.GetComponent<DissolveShaderStart>().ChangeShaderValue());
+        StartCoroutine(texts.transform.Find("Text").gameObject.GetComponent<DissolveShaderStart>().ChangeShaderValue());
+        StartCoroutine(texts.transform.Find("Text2").gameObject.GetComponent<DissolveShaderStart>().ChangeShaderValue());
     }
 }
