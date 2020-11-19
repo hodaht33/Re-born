7632fc971d2210b35a0ec5b2c043b383841a4b22
hodaht33 <hodaht@naver.com>
2020-11-19 12:30:34 +0900

    chap1

--- RebornGameProject/Assets/Scripts/CampusPrototype/CamMoveNextLevel.cs
+++ RebornGameProject/Assets/Scripts/CampusPrototype/CamMoveNextLevel.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CamMoveNextLevel : MonoBehaviour
 {
+    private Camera camera;
     private Transform nextLevelCamPos;
     private bool canMove;
+    private bool canRotate;
+    private int level = 1;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
 
     public void MovePos(Transform nextLevelCamPos)
     {
         this.nextLevelCamPos = nextLevelCamPos;
         canMove = true;
+        canRotate = true;
+        camera.cullingMask = -1 << LayerMask.NameToLayer("Level" + level);
+        level += 1;
+        camera.cullingMask = 1 << LayerMask.NameToLayer("Level" + level);
     }
 
     private void LateUpdate()
@@ -21,6 +33,25 @@ public class CamMoveNextLevel : MonoBehaviour
             || transform.position.z < nextLevelCamPos.position.z))
         {
             transform.position = Vector3.Lerp(transform.position, nextLevelCamPos.position, Time.deltaTime * 2.0f);
+        }
+        else
+        {
+            canMove = false;
+        }
+
+        if (canRotate == true
+            )//&& ())
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.RotateTowards(transform.rotation, nextLevelCamPos.rotation, 2.0f), Time.deltaTime * 5.0f);
+        }
+        else
+        {
+            canRotate = false;
+        }
+
+        if (canMove == false && canRotate == false)
+        {
+            enabled = false;
         }
     }
 }
---
+++ RebornGameProject/Assets/Scripts/Level/SubwayLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubwayLevel : LevelManager
+{
+    public override bool StartLevel()
+    {
+        GetCurrentQuestion().StartQuestion();
+
+        return true;
+    }
+}
