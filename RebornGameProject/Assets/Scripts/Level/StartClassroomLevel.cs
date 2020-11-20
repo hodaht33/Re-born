4a50419762f044370a738db5c8b7595b46444e5d
hodaht33 <hodaht@naver.com>
2020-11-20 13:10:40 +0900

    Chapter1_Subway, Chapter1_Campus

---
+++ RebornGameProject/Assets/Scripts/Level/StartClassroomLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartClassroomLevel : MonoBehaviour
+{
+    [SerializeField] private Transform playerStartPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == true
+            && Inventory.Instance.FindItem("Key") == true)
+        {
+            Camera.main.GetComponent<TreeQuestionCamera>().enabled = false;
+            Camera.main.GetComponent<ClassroomCameraMove>().enabled = true;
+            Camera.main.cullingMask -= 1 << LayerMask.NameToLayer("Level2");
+            Camera.main.cullingMask += 1 << LayerMask.NameToLayer("Level3");
+            other.transform.Rotate(Vector3.up * -90.0f);
+            other.transform.position = playerStartPos.position;
+        }
+    }
+}
