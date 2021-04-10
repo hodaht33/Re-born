6d07566a5dc58062bef40efb7dfd69e6fbd84a7e
unknownpgr <unknownpgr@gmail.com>
2021-04-10 11:36:31 +0900

    Add annotations and optimize code

--- RebornGameProject/Assets/Scripts/01. Subway/Handle.cs
+++ RebornGameProject/Assets/Scripts/01. Subway/Handle.cs
@@ -3,53 +3,55 @@ using UnityEngine.UI;
 
 /// <summary>
 /// 작성자 : 권준호
-/// 기능 : 손잡이 위 아래 이동, 이벤트 발생
+/// 손잡이 위 아래 이동, 이벤트 발생
+/// 손잡이가 클릭될 경우 스스로 내려가고, 내려가는 동작이 완료될 경우 OnHandlePulled 이벤트 발생.
+/// Release 함수 호출 시 손잡이가 다시 위로 올라가며 이때는 아무런 이벤트도 발생하지 않음.
 /// </summary>
 public class Handle : MonoBehaviour
 {
     public HandlePuzzle Parent;
     public int Index;
 
-    private new RectTransform transform;
-    private Vector3 defaultPoistion;
-    private Vector3 destPosition;
+    private Vector3 defaultPoistion;    // The default (not pulled) position of handle.
+    private Vector3 destPosition;       // The position where handle shoulde be.
     private bool isPulled = false;
 
     private void Start()
     {
-        transform = GetComponent<RectTransform>();
         defaultPoistion = transform.position;
         destPosition = defaultPoistion;
+        gameObject.AddComponent<Button>().onClick.AddListener(Pull);
+    }
 
-        Button button = gameObject.AddComponent<Button>();
-        button.onClick.AddListener(OnClicked);
+    public void Pull()
+    {
+        if (isPulled) return;
+        isPulled = true;
+        enabled = true;
+        destPosition = defaultPoistion + new Vector3(0, -40, 0);
     }
 
     public void Release()
     {
         if (!isPulled) return;
-        destPosition = defaultPoistion;
         isPulled = false;
         enabled = true;
-    }
-
-    public void OnClicked()
-    {
-        if (isPulled) return;
-        destPosition = defaultPoistion + new Vector3(0, -40, 0);
-        isPulled = true;
-        enabled = true;
+        destPosition = defaultPoistion;
     }
 
     private void Update()
     {
+        // Note tha tihs function is called only when this script is enabled.
+
         Vector3 pos = transform.position;
         if ((pos - destPosition).magnitude > 1f)
         {
+            // If current position is not close enough to destination, move closer to destination position.
             transform.position = Vector3.Lerp(pos, destPosition, Time.deltaTime * 4);
         }
         else
         {
+            // If current position is close enough to destination, stop this script and invoke pulled event. 
             enabled = false;
             if (isPulled) Parent.OnHandlePulled(Index);
         }
--- RebornGameProject/Assets/Scripts/01. Subway/HandlePuzzle.cs
+++ RebornGameProject/Assets/Scripts/01. Subway/HandlePuzzle.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 
 /// <summary>
 /// 작성자 : 권준호
-/// 기능 : 손잡이 퍼즐 관리
+/// 기능 : 손잡이 퍼즐 관리, 손잡이 gameObject에 동적으로 스크립트를 추가하고 이벤트를 받아 옴.
+/// 손잡이가 모두 당겨졌을 때, 지정된 순서대로 당겨졌다면 그냥 종료하고 그렇지 않다면 그냥 원상복구함.
+/// 한번 손잡이를 모두 당기면 그 이후로는 이벤트가 발생하지 않음.
 /// </summary>
+
 public class HandlePuzzle : Puzzle
 {
-    [SerializeField, Tooltip("손잡이 개수 만큼 써서 사용\n1부터 시작")]
+    [SerializeField, Tooltip("손잡이를 당기는 순서를 나타내는 4자리 비밀번호. (e.g. 1234,1324,1432)")]
     private string answer = "1423";
 
     private readonly int[] answerNumber = new int[4];
-    private Handle[] handles = new Handle[4];
+    private readonly Handle[] handles = new Handle[4];
     private readonly List<int> userAnswer = new List<int>();
     private Canvas canvas;
 
