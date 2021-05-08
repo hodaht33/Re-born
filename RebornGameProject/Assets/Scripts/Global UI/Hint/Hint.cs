e3d5a58d5847c3899a16fdf006b8a8f00ad4a00c
JinsungGwag <dkdjsksj@gmail.com>
2021-05-08 20:53:28 +0900

    Make hint system

---
+++ RebornGameProject/Assets/Scripts/Global UI/Hint/Hint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 작성자 : 곽진성
+/// 기능 : 힌트 오브젝트 관련 기능
+/// </summary>
+public class Hint : MonoBehaviour
+{
+    private void Start()
+    {
+        MousePoint.Instance.pointObject += ClickHint;
+    }
+
+    // 힌트 오브젝트를 클릭했을 경우
+    private void ClickHint(Transform obj)
+    {
+        if(obj == transform)
+        {
+            if (!HintManager.Instance.CheckHintGage())
+                return;
+
+            MousePoint.Instance.pointObject -= ClickHint;
+            HintManager.Instance.StartMoveIcon(MousePoint.Instance.mainCamera.WorldToScreenPoint(obj.position));
+        }
+    }
+}
