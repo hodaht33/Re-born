f62d5b31f7ad05b228f6f0b6dac046f18facabb1
JinsungGwag <dkdjsksj@gmail.com>
2021-07-02 21:21:44 +0900

    Make talk message & item slot event

---
+++ RebornGameProject/Assets/Scripts/04. Man/GetPhone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 남자방 입장시 핸드폰 획득
+/// </summary>
+public class GetPhone : MonoBehaviour
+{
+    [SerializeField] GameObject phone;  // 핸드폰 패널
+    
+    private ItemLSH item;
+
+    private void Start()
+    {
+        // 아이템 슬롯에 핸드폰 표시 이벤트 추가
+        item = GetComponent<ItemLSH>();
+        Inventory.Instance.GetItem(item).GetItemEvent += ShowPhone;
+        Inventory.Instance.StartAndGetCoroutineUpAndDownInventory();
+    }
+
+    // 핸드폰 표시
+    public void ShowPhone()
+    {
+        // 기존 챗창 숨김
+        Chat.Instance.DeactivateChat();
+
+        phone.SetActive(true);
+    }
+}
---
+++ RebornGameProject/Assets/Scripts/04. Man/MessageShow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 카카오톡 메시지 표시 
+/// </summary>
+public class MessageShow : MonoBehaviour
+{
+    [SerializeField] float period;          // 메시지 표시 주기
+    [SerializeField] GameObject[] message;  // 모든 메시지
+
+    private void Start()
+    {
+        Init();
+    }
+
+    // 모든 메시지 숨김
+    private void Init()
+    {
+        foreach (GameObject temp in message)
+        {
+            temp.SetActive(false);
+        }
+    }
+
+    // 모든 메시지 순차적으로 표시
+    private IEnumerator ShowMessage()
+    {
+        // 메시지 순차적으로 표시
+        for(int i = 0; i < message.Length; i++)
+        {
+            yield return new WaitForSeconds(period);
+            message[i].SetActive(true);
+        }
+    }
+}
