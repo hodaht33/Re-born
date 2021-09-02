22e160293a4d7d5ab9f23e3cfee97c3ca8e356f0
Jinsung <jspartner1@naver.com>
2021-09-02 15:37:07 +0900

    Apply lock object

---
+++ RebornGameProject/Assets/Scripts/04. Manroom/MirrorLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 작성자 : 곽진성
+/// 기능 : 미러방 안에서의 자물쇠, 정답 : 1249
+/// </summary>
+public class MirrorLock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
