b1cbb21d32b3e4aff79ff37a6a94c100c990d383
Jinsung <jspartner1@naver.com>
2021-09-09 21:08:44 +0900

    Make lock puzzle in mirror room

--- RebornGameProject/Assets/Scripts/04. Manroom/MirrorLock.cs
+++ RebornGameProject/Assets/Scripts/04. Manroom/MirrorLock.cs
@@ -8,15 +8,36 @@ using UnityEngine;
 /// </summary>
 public class MirrorLock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 자물쇠 버튼 목록
+    [SerializeField] MirrorLockButton[] buttonList;
+    
+    private Animator open;
+
+    private void Awake()
     {
-        
+        open = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        CheckAnswer();
+    }
+
+    // 자물쇠 비밀번호 확인
+    public void CheckAnswer()
+    {
+        if (!buttonList[0].clicked) return;
+        if (!buttonList[1].clicked) return;
+        if (buttonList[2].clicked) return;
+        if (!buttonList[3].clicked) return;
+        if (buttonList[4].clicked) return;
+        if (buttonList[5].clicked) return;
+        if (buttonList[6].clicked) return;
+        if (buttonList[7].clicked) return;
+        if (!buttonList[8].clicked) return;
+        if (buttonList[9].clicked) return;
+
+        // 정답이 맞으면 자물쇠 오픈
+        open.SetTrigger("open");
     }
 }
